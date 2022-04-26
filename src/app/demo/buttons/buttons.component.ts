61b089323dfa8ea0f269e9642d60f00518f519cd
Khajabi Shaikh <khajabi0410@gmail.com>
2022-04-26 08:39:45 +0530

    update for routing

--- src/app/demo/buttons/buttons.component.ts
+++ src/app/demo/buttons/buttons.component.ts
@@ -4,11 +4,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-buttons',
   template: `
-      <button mat-flat-button>
-          <mat-icon>home</mat-icon>
-            Click Me
-      </button>
-      <mat-checkbox>Check me!</mat-checkbox>
+
   `,
   styles: []
 })
