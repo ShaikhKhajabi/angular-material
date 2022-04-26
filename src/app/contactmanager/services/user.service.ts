62285b9f122dc881a69a6ca1951946d1dd07cfd9
Khajabi Shaikh <khajabi0410@gmail.com>
2022-04-26 16:06:00 +0530

    services for data fetching

---
+++ src/app/contactmanager/services/user.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private _users: BehaviorSubject<User[]>;
+
+  private dataStore: {
+    users: User[];
+  };
+
+  constructor(private http: HttpClient) {
+    this._users = new BehaviorSubject<User[]>([]);
+    this.dataStore = { users: [] };
+  }
+ 
+  get users(): Observable<User[]>{
+    return this._users.asObservable();
+  }
+
+  loadAll() {
+    const usersUrl = '';
+
+    return this.http.get<User[]>(usersUrl).subscribe(
+      (data) => {
+        this.dataStore.users = data;
+        this._users.next(Object.assign({}, this.dataStore).users);
+      },
+       error => {
+        console.log('failed to fetch');
+      }
+    );
+  }
+}
