4db17572f705f395a4a35ee54eb3dc100b9006bc
Khajabi Shaikh <khajabi0410@gmail.com>
2022-04-26 16:09:58 +0530

    updates for data service

--- src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -1,25 +1,38 @@
 import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 const SMALL_WIDTH_BREAKPOINT = 720;
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent implements OnInit {
-
   public isScreenSmall: Boolean | undefined;
 
-  constructor(private breakpointObserver: BreakpointObserver) { }
+  users!: Observable<User[]>;
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
     this.breakpointObserver
-    .observe([` (max-width: ${SMALL_WIDTH_BREAKPOINT}px) `])
-    .subscribe((state: BreakpointState) => {
-      this.isScreenSmall = state.matches;
+      .observe([` (max-width: ${SMALL_WIDTH_BREAKPOINT}px) `])
+      .subscribe((state: BreakpointState) => {
+        this.isScreenSmall = state.matches;
+      });
+
+    this.users = this.userService.users;
+    this.userService.loadAll();
+
+    this.users.subscribe((data) => {
+      console.log(data);
     });
   }
-
 }
--- src/app/contactmanager/contactmanager.module.ts
+++ src/app/contactmanager/contactmanager.module.ts
@@ -11,6 +11,8 @@ import { ContactmanagerAppComponent } from './contactmanager-app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { UserService } from './services/user.service';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
@@ -33,10 +35,14 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
+    HttpClientModule,
     MaterialModule,
     FlexLayoutModule,
     FormsModule,
     RouterModule.forChild(routes)
+  ],
+  providers: [
+    UserService
   ]
 })
 export class ContactmanagerModule { }
